da943f29570de7a6efdf37f166613b426fa929b7
0chil <0@chll.it>
2022-05-05 13:58:22 +0900

    chore: interface UserRepository @Repository 추가

--- src/main/java/com/swp/user/domain/UserRepository.java
+++ src/main/java/com/swp/user/domain/UserRepository.java
@@ -3,7 +3,9 @@ package com.swp.user.domain;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
 	Optional<User> findById(Integer id);
