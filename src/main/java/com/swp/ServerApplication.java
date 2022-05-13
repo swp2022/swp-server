77e7ad94149c7c391d70fca72dfdc454863515de
LEEYEONGJAE1 <lyj8520675@naver.com>
2022-05-13 17:38:24 +0900

    feat: 팔로워/팔로잉 기능 구현

--- src/main/java/com/swp/ServerApplication.java
+++ src/main/java/com/swp/ServerApplication.java
@@ -3,7 +3,7 @@ package com.swp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
