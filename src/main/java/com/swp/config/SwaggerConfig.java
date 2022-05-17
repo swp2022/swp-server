98e95ed4f745d28fec0412c607de36d70fe52097
0chil <0@chll.it>
2022-05-18 01:43:12 +0900

    feat: Swagger API 인증 추가

--- src/main/java/com/swp/config/SwaggerConfig.java
+++ src/main/java/com/swp/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.swp.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,15 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.AuthorizationScopeBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.DocExpansion;
 import springfox.documentation.swagger.web.UiConfiguration;
@@ -24,6 +31,12 @@ public class SwaggerConfig implements WebMvcConfigurer {
 	private static final String API_TITLE = "OStudy API";
 	private static final String API_DESCRIPTION = "OStudy API Document";
 	private static final String API_VERSION = "Version 1";
+	private static final String SWAGGER_PATH = "/swagger-ui/index.html";
+	private static final String API_KEY_HEADER_NAME = "Authorization";
+	private static final String API_KEY_IN = "header";
+	private static final String API_KEY_NAME = "JWT AccessToken";
+	private static final String SCOPE_GLOBAL = "global";
+	private static final String SCOPE_GLOBAL_DESCRIPTION = "to access resources";
 
 	private ApiInfo createApiInfo() {
 		return new ApiInfoBuilder()
@@ -44,6 +57,8 @@ public class SwaggerConfig implements WebMvcConfigurer {
 	public Docket docket() {
 		return new Docket(DocumentationType.OAS_30)
 			.apiInfo(createApiInfo())
+			.securityContexts(List.of(getSecurityContext()))
+			.securitySchemes(List.of(getApiKey()))
 			.select()
 			.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
 			.paths(PathSelectors.any())
@@ -52,7 +67,30 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addRedirectViewController("/docs", "/swagger-ui/index.html");
-		registry.addRedirectViewController("/swagger-ui", "/swagger-ui/index.html");
+		registry.addRedirectViewController("/docs", SWAGGER_PATH);
+		registry.addRedirectViewController("/swagger-ui", SWAGGER_PATH);
 	}
+
+	private ApiKey getApiKey() {
+		return new ApiKey(API_KEY_HEADER_NAME, API_KEY_NAME, API_KEY_IN);
+	}
+
+	private SecurityContext getSecurityContext() {
+		return SecurityContext.builder()
+			.securityReferences(getSecurityReferences())
+			.build();
+	}
+
+	private List<SecurityReference> getSecurityReferences() {
+		return List.of(new SecurityReference(API_KEY_HEADER_NAME, getAuthorizationScopes()));
+	}
+
+	private AuthorizationScope[] getAuthorizationScopes() {
+		AuthorizationScope globalScope = new AuthorizationScopeBuilder()
+			.scope(SCOPE_GLOBAL)
+			.description(SCOPE_GLOBAL_DESCRIPTION)
+			.build();
+		return new AuthorizationScope[] {globalScope};
+	}
+
 }
