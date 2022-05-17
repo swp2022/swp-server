c0cf0be3a01f0ccfe223eacbea8dc9e35f3c37e0
LEEYEONGJAE1 <lyj8520675@naver.com>
2022-05-18 00:11:30 +0900

    feat:게시글(Study 정보 없이)

--- src/main/java/com/swp/user/controller/RelationshipController.java
+++ src/main/java/com/swp/user/controller/RelationshipController.java
@@ -1,12 +1,10 @@
 package com.swp.user.controller;
 
-import com.swp.auth.dto.JwtUserDetails;
 import com.swp.user.domain.RelationshipService;
 import com.swp.user.dto.RelationshipRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -18,18 +16,12 @@ public class RelationshipController {
     @PostMapping(value = "/v1/relationship")
     @ResponseStatus(HttpStatus.CREATED)
     public void followUser(@RequestBody RelationshipRequestDto relationshipRequestDto){
-        JwtUserDetails userDetails = (JwtUserDetails) SecurityContextHolder.getContext()
-                .getAuthentication()
-                .getPrincipal();
         relationshipService.createRelationship(relationshipRequestDto);
     }
 
     @DeleteMapping(value = "/v1/relationship")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void unfollowUser(@RequestBody RelationshipRequestDto relationshipRequestDto){
-        JwtUserDetails userDetails = (JwtUserDetails)SecurityContextHolder.getContext()
-                .getAuthentication()
-                .getPrincipal();
         relationshipService.deleteRelationship(relationshipRequestDto);
     }
 }
