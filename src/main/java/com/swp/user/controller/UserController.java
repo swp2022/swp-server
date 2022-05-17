07087d4cfff1c302c5edca0b4fb5e29f25e9461b
LEEYEONGJAE1 <lyj8520675@naver.com>
2022-05-17 14:55:57 +0900

    feat:게시글(Study 정보 없이)

--- src/main/java/com/swp/user/controller/UserController.java
+++ src/main/java/com/swp/user/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.swp.user.controller;
 
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.swp.auth.dto.JwtUserDetails;
 import com.swp.user.domain.UserService;
