f665b6daf1b4f7a2b858f06266b8381c3f6fcf89
LEEYEONGJAE1 <lyj8520675@naver.com>
2022-05-18 23:45:22 +0900

    feat:팔로우하는 유저 글 전부 페이징 해서 가져오기

--- src/main/java/com/swp/user/domain/Relationship.java
+++ src/main/java/com/swp/user/domain/Relationship.java
@@ -15,6 +15,7 @@ public class Relationship {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "relationship_id", nullable = false)
     private Integer id;
+    
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="from_user_id",nullable = false)
