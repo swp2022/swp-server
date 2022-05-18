8774618f2d7cdb41e92536a9741a2cd1df807970
leeyeongjae <lyj8520675@naver.com>
2022-05-18 18:32:04 +0900

    feat:팔로우하는 유저 글 전부 페이징 해서 가져오기

--- src/main/java/com/swp/board/domain/BoardRepository.java
+++ src/main/java/com/swp/board/domain/BoardRepository.java
@@ -3,7 +3,6 @@ package com.swp.board.domain;
 import com.swp.user.domain.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
