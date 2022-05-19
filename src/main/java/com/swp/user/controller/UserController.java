0e04335da49297e399cf1f314f853a785aca43c8
0chil <0@chll.it>
2022-05-19 22:19:07 +0900

    feat: 팔로워, 팔로잉 가져오기

--- src/main/java/com/swp/user/controller/UserController.java
+++ src/main/java/com/swp/user/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.swp.user.controller;
 
+import java.util.List;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.swp.auth.dto.JwtUserDetails;
 import com.swp.user.domain.UserService;
 import com.swp.user.dto.UserResponseDto;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -23,5 +29,35 @@ public class UserController {
 			.getAuthentication()
 			.getPrincipal();
 		return userService.getUser(userDetails);
+	}
+
+	@ApiOperation(value = "자신의 팔로워 가져오기", notes = "자신**을** 팔로잉하는 사람들")
+	@GetMapping(value = "/followers")
+	public List<UserResponseDto> getMyFollowers() {
+		JwtUserDetails userDetails = (JwtUserDetails)SecurityContextHolder.getContext()
+			.getAuthentication()
+			.getPrincipal();
+		return userService.getFollowers(userDetails);
+	}
+
+	@ApiOperation(value = "자신의 팔로잉 가져오기", notes = "자신**이** 팔로잉하는 사람들")
+	@GetMapping(value = "/followings")
+	public List<UserResponseDto> getMyFollowings() {
+		JwtUserDetails userDetails = (JwtUserDetails)SecurityContextHolder.getContext()
+			.getAuthentication()
+			.getPrincipal();
+		return userService.getFollowings(userDetails);
+	}
+
+	@ApiOperation(value = "유저의 팔로워 가져오기", notes = "유저**를** 팔로잉하는 사람들")
+	@GetMapping(value = "{userId}/followers")
+	public List<UserResponseDto> getFollowers(@PathVariable Integer userId) {
+		return userService.getFollowers(userId);
+	}
+
+	@ApiOperation(value = "유저의 팔로잉 가져오기", notes = "유저**가** 팔로잉하는 사람들")
+	@GetMapping(value = "{userId}/followings")
+	public List<UserResponseDto> getFollowings(@PathVariable Integer userId) {
+		return userService.getFollowings(userId);
 	}
 }
--- src/main/java/com/swp/user/domain/UserService.java
+++ src/main/java/com/swp/user/domain/UserService.java
@@ -1,9 +1,12 @@
 package com.swp.user.domain;
 
-import java.util.NoSuchElementException;
+import static java.util.stream.Collectors.*;
+
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.swp.auth.dto.JwtUserDetails;
 import com.swp.user.dto.UserResponseDto;
 import com.swp.user.exception.UserNotFoundException;
 
@@ -19,5 +22,41 @@ public class UserService {
 		User user = userRepository.findByProviderAndProviderId(userDetails.getProvider(), userDetails.getUsername())
 			.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
 		return UserResponseDto.from(user);
+	}
+
+	public List<UserResponseDto> getFollowers(JwtUserDetails userDetails) {
+		User user = userRepository.findByProviderAndProviderId(userDetails.getProvider(), userDetails.getUsername())
+			.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return user.getFollowerList().stream()
+			.map(Relationship::getFromUser)
+			.map(UserResponseDto::from)
+			.collect(toList());
+	}
+
+	public List<UserResponseDto> getFollowers(Integer userId) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return user.getFollowerList().stream()
+			.map(Relationship::getFromUser)
+			.map(UserResponseDto::from)
+			.collect(toList());
+	}
+
+	public List<UserResponseDto> getFollowings(JwtUserDetails userDetails) {
+		User user = userRepository.findByProviderAndProviderId(userDetails.getProvider(), userDetails.getUsername())
+			.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return user.getFollowingList().stream()
+			.map(Relationship::getToUser)
+			.map(UserResponseDto::from)
+			.collect(toList());
+	}
+
+	public List<UserResponseDto> getFollowings(Integer userId) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return user.getFollowingList().stream()
+			.map(Relationship::getToUser)
+			.map(UserResponseDto::from)
+			.collect(toList());
 	}
 }
