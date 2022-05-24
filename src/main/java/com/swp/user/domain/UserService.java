012ae82d66cb222b718ff5b450f7bdba14e5287f
LEEYEONGJAE1 <lyj8520675@naver.com>
2022-05-24 20:13:48 +0900

    feat:nickname으로 유저 검색

--- src/main/java/com/swp/user/domain/UserService.java
+++ src/main/java/com/swp/user/domain/UserService.java
@@ -18,58 +18,58 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class UserService {
 
-        private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-        public UserResponseDto getUser(JwtUserDetails userDetails) {
-            User user = userRepository.findByProviderAndProviderId(userDetails.getProvider(), userDetails.getUsername())
-                    .orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
-            return UserResponseDto.from(user);
-        }
+	public UserResponseDto getUser(JwtUserDetails userDetails) {
+		User user = userRepository.findByProviderAndProviderId(userDetails.getProvider(), userDetails.getUsername())
+				.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return UserResponseDto.from(user);
+	}
 
-        @Transactional(readOnly = true)
-        public List<UserResponseDto> getFollowers(JwtUserDetails userDetails) {
-            User user = userRepository.findByProviderAndProviderId(userDetails.getProvider(), userDetails.getUsername())
-                    .orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
-            return user.getFollowerList().stream()
-                    .map(Relationship::getFromUser)
-                    .map(UserResponseDto::from)
-                    .collect(toList());
-        }
+	@Transactional(readOnly = true)
+	public List<UserResponseDto> getFollowers(JwtUserDetails userDetails) {
+		User user = userRepository.findByProviderAndProviderId(userDetails.getProvider(), userDetails.getUsername())
+				.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return user.getFollowerList().stream()
+				.map(Relationship::getFromUser)
+				.map(UserResponseDto::from)
+				.collect(toList());
+	}
 
-        @Transactional(readOnly = true)
-        public List<UserResponseDto> getFollowers(Integer userId) {
-            User user = userRepository.findById(userId)
-                    .orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
-            return user.getFollowerList().stream()
-                    .map(Relationship::getFromUser)
-                    .map(UserResponseDto::from)
-                    .collect(toList());
-        }
+	@Transactional(readOnly = true)
+	public List<UserResponseDto> getFollowers(Integer userId) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return user.getFollowerList().stream()
+				.map(Relationship::getFromUser)
+				.map(UserResponseDto::from)
+				.collect(toList());
+	}
 
-        @Transactional(readOnly = true)
-        public List<UserResponseDto> getFollowings(JwtUserDetails userDetails) {
-            User user = userRepository.findByProviderAndProviderId(userDetails.getProvider(), userDetails.getUsername())
-                    .orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
-            return user.getFollowingList().stream()
-                    .map(Relationship::getToUser)
-                    .map(UserResponseDto::from)
-                    .collect(toList());
-        }
+	@Transactional(readOnly = true)
+	public List<UserResponseDto> getFollowings(JwtUserDetails userDetails) {
+		User user = userRepository.findByProviderAndProviderId(userDetails.getProvider(), userDetails.getUsername())
+				.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return user.getFollowingList().stream()
+				.map(Relationship::getToUser)
+				.map(UserResponseDto::from)
+				.collect(toList());
+	}
 
-        @Transactional(readOnly = true)
-        public List<UserResponseDto> getFollowings(Integer userId) {
-            User user = userRepository.findById(userId)
-                    .orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
-            return user.getFollowingList().stream()
-                    .map(Relationship::getToUser)
-                    .map(UserResponseDto::from)
-                    .collect(toList());
-        }
+	@Transactional(readOnly = true)
+	public List<UserResponseDto> getFollowings(Integer userId) {
+		User user = userRepository.findById(userId)
+				.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return user.getFollowingList().stream()
+				.map(Relationship::getToUser)
+				.map(UserResponseDto::from)
+				.collect(toList());
+	}
 
-        @Transactional(readOnly = true)
-        public UserResponseDto getUserByNickname(String nickname) {
-            User user = userRepository.findByNickname(nickname)
-                    .orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
-            return UserResponseDto.from(user);
-        }
+	@Transactional(readOnly = true)
+	public UserResponseDto getUserByNickname(String nickname) {
+		User user = userRepository.findByNickname(nickname)
+				.orElseThrow(() -> new UserNotFoundException("없는 유저입니다"));
+		return UserResponseDto.from(user);
+	}
 }
