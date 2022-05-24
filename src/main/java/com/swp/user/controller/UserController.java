d5dc2aff936f0a9cfbc06d44ed2c956cffd6f8fb
LEEYEONGJAE1 <lyj8520675@naver.com>
2022-05-24 20:12:05 +0900

    feat:nickname으로 유저 검색

--- src/main/java/com/swp/user/controller/UserController.java
+++ src/main/java/com/swp/user/controller/UserController.java
@@ -20,46 +20,50 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class UserController {
 
-	private final UserService userService;
+    private final UserService userService;
 
-	@ApiOperation("자신의 정보 가져오기")
-	@GetMapping
-	public UserResponseDto getUser() {
-		JwtUserDetails userDetails = (JwtUserDetails)SecurityContextHolder.getContext()
-			.getAuthentication()
-			.getPrincipal();
-		return userService.getUser(userDetails);
-	}
+    @ApiOperation("자신의 정보 가져오기")
+    @GetMapping
+    public UserResponseDto getUser() {
+        JwtUserDetails userDetails = (JwtUserDetails) SecurityContextHolder.getContext()
+            .getAuthentication()
+            .getPrincipal();
+        return userService.getUser(userDetails);
+    }
 
-	@ApiOperation(value = "자신의 팔로워 가져오기", notes = "자신**을** 팔로잉하는 사람들")
-	@GetMapping(value = "/followers")
-	public List<UserResponseDto> getMyFollowers() {
-		JwtUserDetails userDetails = (JwtUserDetails)SecurityContextHolder.getContext()
-			.getAuthentication()
-			.getPrincipal();
-		return userService.getFollowers(userDetails);
-	}
+    @ApiOperation(value = "자신의 팔로워 가져오기", notes = "자신**을** 팔로잉하는 사람들")
+    @GetMapping(value = "/followers")
+    public List<UserResponseDto> getMyFollowers() {
+        JwtUserDetails userDetails = (JwtUserDetails) SecurityContextHolder.getContext()
+            .getAuthentication()
+            .getPrincipal();
+        return userService.getFollowers(userDetails);
+    }
 
-	@ApiOperation(value = "자신의 팔로잉 가져오기", notes = "자신**이** 팔로잉하는 사람들")
-	@GetMapping(value = "/followings")
-	public List<UserResponseDto> getMyFollowings() {
-		JwtUserDetails userDetails = (JwtUserDetails)SecurityContextHolder.getContext()
-			.getAuthentication()
-			.getPrincipal();
-		return userService.getFollowings(userDetails);
-	}
+    @ApiOperation(value = "자신의 팔로잉 가져오기", notes = "자신**이** 팔로잉하는 사람들")
+    @GetMapping(value = "/followings")
+    public List<UserResponseDto> getMyFollowings() {
+        JwtUserDetails userDetails = (JwtUserDetails) SecurityContextHolder.getContext()
+            .getAuthentication()
+            .getPrincipal();
+        return userService.getFollowings(userDetails);
+    }
 
-	@ApiOperation(value = "유저의 팔로워 가져오기", notes = "유저**를** 팔로잉하는 사람들")
-	@GetMapping(value = "{userId}/followers")
-	public List<UserResponseDto> getFollowers(@PathVariable Integer userId) {
-		return userService.getFollowers(userId);
-	}
+    @ApiOperation(value = "유저의 팔로워 가져오기", notes = "유저**를** 팔로잉하는 사람들")
+    @GetMapping(value = "{userId}/followers")
+    public List<UserResponseDto> getFollowers(@PathVariable Integer userId) {
+        return userService.getFollowers(userId);
+    }
 
-	@ApiOperation(value = "유저의 팔로잉 가져오기", notes = "유저**가** 팔로잉하는 사람들")
-	@GetMapping(value = "{userId}/followings")
-	public List<UserResponseDto> getFollowings(@PathVariable Integer userId) { return userService.getFollowings(userId); }
+    @ApiOperation(value = "유저의 팔로잉 가져오기", notes = "유저**가** 팔로잉하는 사람들")
+    @GetMapping(value = "{userId}/followings")
+    public List<UserResponseDto> getFollowings(@PathVariable Integer userId) {
+        return userService.getFollowings(userId);
+    }
 
-	@ApiOperation(value="nickname으로 유저 검색하기")
-	@GetMapping(value="/search/{nickname}")
-	public UserResponseDto getUserByNickname(@PathVariable String nickname){ return userService.getUserByNickname(nickname); }
+    @ApiOperation(value = "nickname으로 유저 검색하기")
+    @GetMapping(value = "/search/{nickname}")
+    public UserResponseDto getUserByNickname(@PathVariable String nickname) {
+        return userService.getUserByNickname(nickname);
+    }
 }
