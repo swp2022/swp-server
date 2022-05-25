1c838d708d9d7e4aaf80df1c21bd3b82b14e51cd
LEEYEONGJAE1 <lyj8520675@naver.com>
2022-05-26 01:21:33 +0900

    pageable 적용

--- src/main/java/com/swp/user/controller/UserController.java
+++ src/main/java/com/swp/user/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.swp.auth.dto.JwtUserDetails;
 import com.swp.user.domain.UserService;
