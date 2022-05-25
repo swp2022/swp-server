30bb8fa621babd3013b692463961dfde27ad1aaf
leeyeongjae <lyj8520675@naver.com>
2022-05-25 18:15:50 +0900

    feat: study logs 조회

--- src/main/java/com/swp/study/domain/StudyService.java
+++ src/main/java/com/swp/study/domain/StudyService.java
@@ -1,2 +1,25 @@
-package com.swp.study.domain;public class StudyService {
+package com.swp.study.domain;
+
+
+import com.swp.study.Exception.StudyNotFoundException;
+import com.swp.study.dto.StudyLogResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class StudyService {
+	private final StudyRepository studyRepository;
+	private final StudyLogRepository studyLogRepository;
+
+	public List<StudyLogResponseDto> getStudyLog(Integer studyId) {
+		Study study = studyRepository.findById(studyId)
+				.orElseThrow(() -> new StudyNotFoundException("study 정보가 없습니다."));
+		return studyLogRepository.findByStudy(study).stream()
+				.map(StudyLogResponseDto::from)
+				.collect(Collectors.toList());
+	}
 }
--- src/main/java/com/swp/study/dto/StudyLogResponseDto.java
+++ src/main/java/com/swp/study/dto/StudyLogResponseDto.java
@@ -1,2 +1,28 @@
-package com.swp.study.dto;public class StudyLogResponseDto {
+package com.swp.study.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.swp.study.domain.StudyLog;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudyLogResponseDto {
+	private Integer studyLogId;
+	private Long recordedTime;
+	private Double percentage;
+
+	public static StudyLogResponseDto from(StudyLog studyLog) {
+		return StudyLogResponseDto.builder()
+				.studyLogId(studyLog.getStudyLogId())
+				.recordedTime(studyLog.getRecordedTime())
+				.percentage(studyLog.getPercentage())
+				.build();
+	}
 }
