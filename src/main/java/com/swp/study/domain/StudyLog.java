ea27723cf6ed701483055753a8282de4302befc1
leeyeongjae <lyj8520675@naver.com>
2022-05-25 18:17:24 +0900

    feat: study logs 조회

--- src/main/java/com/swp/study/domain/StudyLog.java
+++ src/main/java/com/swp/study/domain/StudyLog.java
@@ -8,18 +8,18 @@ import javax.persistence.*;
 @Table(name = "study_logs")
 @Getter
 public class StudyLog {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "study_log_id", nullable = false)
-	private Integer studyLogId;
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		@Column(name = "study_log_id", nullable = false)
+		private Integer studyLogId;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "study_id", nullable = false)
-	private Study study;
+		@ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "study_id", nullable = false)
+		private Study study;
 
-	@Column(name = "recorded_time", nullable = false)
-	private Long recordedTime;
+		@Column(name = "recorded_time", nullable = false)
+		private Long recordedTime;
 
-	@Column(name = "percentage", nullable = false)
-	private Double percentage;
+		@Column(name = "percentage", nullable = false)
+		private Double percentage;
 }
--- src/main/java/com/swp/study/domain/StudyRepository.java
+++ src/main/java/com/swp/study/domain/StudyRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyRepository extends JpaRepository<Study, Integer> {
+	
 }
